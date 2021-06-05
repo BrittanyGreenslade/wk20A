51c13497edd9dcca3c3b4682557eeea32aa7baf7
Brittany <bgreensl@ualberta.ca>
2021-06-04 21:06:17 -0600

    added some exceptions

--- app.py
+++ app.py
@@ -8,46 +8,58 @@ cursor = conn.cursor()
 cursor.execute("SELECT * FROM blog_post")
 
 
-def getUsername():
+def get_username():
     return input("Please enter your username: ")
 
 
-def getPosts():
+def get_posts():
     cursor.execute("SELECT username, content FROM blog_post")
     all_posts = cursor.fetchall()
     print(all_posts)
 
 
-username = getUsername()
-selection = int(input(
-    f"Hello {username}. Please select from the following options: \n 1. Make a post \n 2. See all posts \n Your selection: "))
+username = get_username()
 
 
-def createPost():
+def create_post():
     cursor.execute(
-        f"INSERT INTO blog_post(username, content) VALUES('{username}','{postContent}')")
+        f"INSERT INTO blog_post(username, content) VALUES('{username}','{post_content}')")
     conn.commit()
 
 
-try:
-    if selection == 1:
-        postContent = input("Please write your blog post below:\n")
-        if postContent:
-            createPost()
-    elif selection == 2:
-        getPosts()
-except:
-    print("Sorry something went wrong")
-    traceback.print_exc()
+while True:
+    try:
+        selection = int(input(
+            f"Hello {username}. Please select from the following options: \n 1. Make a post \n 2. See all posts \n Your selection: "))
+        if selection == 1:
+            post_content = input("Please write your blog post below:\n")
+            # if post_content is input by the user, run the fn create_post
+            if post_content:
+                create_post()
+        elif selection == 2:
+            get_posts()
+
+        else:
+            print("I specifically said 1 or 2.... :)")
+            continue
+        break
+    except ValueError:
+        # this else makes ctrl c stop working in the terminal after I input a letter...
+        # but.... it doesn't matter since we won't be using the terminal for this IRL right?
+        print("Plz... just enter 1 or 2.....")
+    except:
+        print("Sorry something went wrong")
+        traceback.print_exc()
 
 
 # if try cursor.close is here (outside of above try block),
-# we ALWAYS try to close it. so the actually connection closes, even if above errors
+# we ALWAYS try to close it. so the cursor actually closes, even if above errors
 try:
     cursor.close()
 except:
     print("Error in closing cursor")
     traceback.print_exc()
+    # same not as above
 try:
     conn.close()
 except:
